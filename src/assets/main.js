cb75f3869faaf19e3e402ea6482b7fee8649e985
AchPanCS <a.p@a.nl>
2017-05-24 13:53:51 +0200

    a
    a

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,9 +3,16 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    if (answer.value = '' && attempt.value = '') {
+
+    if (answer.value = '' || attempt.value = '') {
       setHiddenFields();
     }
+
+    if(!validateInput(input.value)){
+      return;
+    }
+    attempt.value++;
+
 }
 
 function setHiddenFields() {
@@ -16,15 +23,32 @@ function setHiddenFields() {
   attempt.value = 0;
 }
 
-function setMessage(param) {
-  message.innerHTML = param;
+function setMessage(message) {
+  document.getElementById('message').innerHTML = message;
 }
 
 function validateInput(input){
-  if (input.length == 4){
-    return true;
-  } else {
-    setMessage("Guesses must be exactly 4 characters long.");
+  if (input.length != 4){
+    setMessage('Guesses must be exactly 4 characters long.');
     return false;
   }
+  return true;
+}
+
+function getResults(input){
+  let result = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">'
+  for ( i = 0; i < input.length; i++){
+    let temp = '';
+    if(answer.value.includes(input[i])){
+      temp = '<span class="glyphicon glyphicon-transfer"></span>';
+      if(input[i] == answer.value[i]){
+        temp = '<span class="glyphicon glyphicon-ok"></span>';
+      }
+    } else {
+      temp = '<span class="glyphicon glyphicon-remove"></span>';
+    };
+    result.concat(temp);
+  };
+  result.concat('</div>')
+  document.getElementById('results').innerHTML = result;
 }
